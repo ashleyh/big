8a263972664b350629178b1db5f6555544457d06
aph <a.hewson@gmail.com>
2014-04-05 23:15:06 +0100

    implement toHexString

--- big.ts
+++ big.ts
@@ -2,6 +2,13 @@ function isInt32(n: number) {
   return (n & 0xffffffff) == n;
 }
 
+function padLeft(s: string) {
+  while (s.length < 8) {
+    s = '0' + s;
+  }
+  return s;
+}
+
 export class Unsigned {
   constructor(public limbs: Uint32Array) {
   }
@@ -18,5 +25,17 @@ export class Unsigned {
     if (this.limbs.length == 1) {
       return this.limbs[0];
     }
+  }
+
+  toHexString() {
+    var result = '',
+        limb: string;
+    for (var i = 0; i < this.limbs.length - 1; i++) {
+      limb = this.limbs[i].toString(16);
+      result = padLeft(limb) + result;
+    }
+    limb = this.limbs[this.limbs.length - 1].toString(16);
+    result = limb + result;
+    return result;
   }
 }
--- test/test.js
+++ test/test.js
@@ -7,4 +7,19 @@ describe('Unsigned', function() {
     var b = big.Unsigned.fromNumber(n);
     assert.equal(b.toNumber(), n);
   });
+
+  it('should produce correct hex representation for small numbers', function() {
+    var a = new Uint32Array(1);
+    a[0] = 0x123;
+    var b = new big.Unsigned(a);
+    assert.equal(b.toHexString(), '123');
+  });
+
+  it('should produce correct hex representation for big numbers', function() {
+    var a = new Uint32Array(2);
+    a[0] = 0x123;
+    a[1] = 0xdef;
+    var b = new big.Unsigned(a);
+    assert.equal(b.toHexString(), 'def00000123');
+  });
 });
